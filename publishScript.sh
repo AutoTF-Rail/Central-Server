7ff9deccc508696ba0aed6a302574c64bedccaa3
chiouya <chiouyazo@gmail.com>
2025-01-26 11:48:46 +0100

    updated ip in publish script

--- publishScript.sh
+++ publishScript.sh
@@ -1,4 +1,4 @@
 dotnet publish -c Release -o out
-docker login 172.17.0.3:5001
-docker build -t 172.17.0.3:5001/repository/docker-autotf/central-server:latest .
-docker push 172.17.0.3:5001/repository/docker-autotf/central-server:latest
+docker login 172.17.0.8:5001
+docker build -t 172.17.0.8:5001/repository/docker-autotf/central-server:latest .
+docker push 172.17.0.8:5001/repository/docker-autotf/central-server:latest
