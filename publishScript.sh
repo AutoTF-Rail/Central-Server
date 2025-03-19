28cca6bf20b66d69f248b3512716bf1ed57ad115
chiouya <chiouyazo@gmail.com>
2025-03-19 14:46:34 +0100

    added publishscript disclaimer

--- publishScript.sh
+++ publishScript.sh
@@ -1,3 +1,4 @@
+# This script is used on the server that hosts the central servers.
 dotnet publish -c Release -o out
 docker login 172.17.0.8:5001
 docker build -t 172.17.0.8:5001/repository/docker-autotf/central-server:latest .
