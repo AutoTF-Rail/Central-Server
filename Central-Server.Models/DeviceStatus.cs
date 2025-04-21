b2f6b51e199be2d9479604dac06fc7b4dd92fafd
chiouya <chiouyazo@gmail.com>
2025-04-21 14:49:13 +0200

    Added object Id to Keydata and device status

--- Central-Server.Models/DeviceStatus.cs
+++ Central-Server.Models/DeviceStatus.cs
@@ -1,7 +1,14 @@
+using LiteDB;
+
 namespace Central_Server.Models;
 
 public class DeviceStatus
 {
+	// LiteDB requires this
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+	public DeviceStatus() {}
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
 	public DeviceStatus(Guid trainId, DateTime timestamp, string status)
 	{
 		TrainId = trainId;
@@ -12,4 +19,6 @@ public class DeviceStatus
 	public DateTime Timestamp { get; set; }
 	public string Status { get; set; }
 	public Guid TrainId { get; set; }
+
+	public ObjectId Id { get; set; } = null!;
 }
